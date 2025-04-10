5cc3ea326d48fac211a915d2cfaf64b25b530574
tomasnovoa <kakoliin004@gmail.com>
2025-04-10 11:59:40 -0400

    listo visualmente

--- Extra.js
+++ Extra.js
@@ -8,3 +8,20 @@ function draw(){
     rect(random(width), random(height), 2, 2);
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const botonesAgotado = document.querySelectorAll(".btn.agotado");
+  const mensaje = document.getElementById("agotadoMsg");
+
+  botonesAgotado.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+      e.preventDefault(); // Para evitar navegación si hay un href
+
+      mensaje.style.display = "block";
+
+      setTimeout(() => {
+        mensaje.style.display = "none";
+      }, 2500);
+    });
+  });
+});
